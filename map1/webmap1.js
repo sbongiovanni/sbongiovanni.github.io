f80dd42690dde45eff60ff6c3c58fdcaae643835
Sarah Bongiovanni <48561808+sbongiovanni@users.noreply.github.com>
2019-03-25 23:01:15 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,7 +1,7 @@
-let myMap = L.map('map1').setView([32.18, -99.14], 4)
+let myMap = L.map('map1').setView([30, -90], 6)
 //L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
 var OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-	maxZoom: 18,
+	maxZoom: 10,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 let nolaPoint = L.marker([29.958941, -90.064618]).addTo(myMap)
