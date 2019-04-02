d31ca66cb6988f104371fb28c3a8ed7cbcc0cf31
Sarah Bongiovanni <sbongi3@lsu.edu>
2019-04-02 15:53:36 -0500

    updated

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,13 +1,13 @@
 let us = L.map('webmap2').setView([38, -100], 4.4);
 	L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(us);
 	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer?request=GetCapabilities&service=WMS', {
-  		layers: 'sat_meteo_emulated_imagery_lightningstrikedensity_goes_time',
+  		layers: '1',
   		format: 'image/png',
   		transparent: true,
   		attribution: 'NOAA'
 	}).addTo(us)
 	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer?request=GetCapabilities&service=WMS', {
- 		 layers: '1',
+ 		 layers: '2',
  		 format: 'image/png',
   		transparent: true,
   attribution: 'NOAA'
