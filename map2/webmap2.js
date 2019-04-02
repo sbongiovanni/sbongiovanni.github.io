02ed357d5db40e3500585e9ffcfa056cfdcca7c6
Sarah Bongiovanni <Sarah@a4-5e-60-ec-4a-a5.wlan.lsu.edu>
2019-04-02 13:35:27 -0500

    updated

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,20 +1,12 @@
-let myMap = L.map('map1').setView([30, -90], 10)
+let myMap = L.map('map1').setView([30, -90], 5)
 //L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
 var OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
-let nolaPoint = L.marker([29.958941, -90.064618]).addTo(myMap)
-let myPolygon = L.polygon([
-  [30.051098, -90.148126],
-  [29.914709, -90.266568],
-  [29.917677, -90.001626],
-]).addTo(myMap);
-let MetairieNolaLine = L.polygon([
-  [30.031052, -90.119940],
-  [29.909986, -90.151926]
-]).addTo(myMap);
-
-myPolygon.bindPopup('New Orleans Metro Area')
-nolaPoint.bindPopup('<strong>French Quarter</strong>')
-MetairieNolaLine.bindPopup ('City Line')
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(myMap)
