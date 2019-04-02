8230b4b0188ba73d8536fa74d4ce8882579c48f6
Sarah Bongiovanni <Sarah@a4-5e-60-ec-4a-a5.wlan.lsu.edu>
2019-04-02 14:26:20 -0500

    updated

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,12 +1,14 @@
-let myMap = L.map('map1').setView([30, -90], 5)
-//L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
-var OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+let us = L.map('webmap2').setView([38, -100], 4.4);
+	L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(us);
+	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer?request=GetCapabilities&service=WMS', {
+  		layers: 'sat_meteo_emulated_imagery_lightningstrikedensity_goes_time',
+  		format: 'image/png',
+  		transparent: true,
+  		attribution: 'NOAA'
+	}).addTo(myMap)
+	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer?request=GetCapabilities&service=WMS', {
+ 		 layers: 'radar_meteo_imagery_nexrad_time',
+ 		 format: 'image/png',
+  		transparent: true,
+  attribution: 'NOAA'
 }).addTo(myMap);
-L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-  layers: 'nexrad-n0r-900913',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA, Iowa State University'
-}).addTo(myMap)
