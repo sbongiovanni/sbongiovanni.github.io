59899c40e530a16f94d4b32654d67db90e79f6f7
Sarah Bongiovanni <48561808+sbongiovanni@users.noreply.github.com>
2019-04-02 20:23:09 -0500

    Update webmap2.js

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -6,8 +6,8 @@ let us = L.map('webmap2').setView([38, -100], 4.4);
   		transparent: true,
   		attribution: 'NOAA, Iowa State University'
 	}).addTo(us)
-	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer?request=GetCapabilities&service=WMS', {
- 		 layers: '2',
+	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_ocean_sfc_sst_time/MapServer/WMSServer?request=GetCapabilities&service=WMS', {
+ 		 layers: '1',
  		 format: 'image/png',
   		transparent: true,
   attribution: 'NOAA'
