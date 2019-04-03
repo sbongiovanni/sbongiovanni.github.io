286a65d85170a5a5a0f633bba73da441928eb74e
Sarah Bongiovanni <48561808+sbongiovanni@users.noreply.github.com>
2019-04-02 20:17:51 -0500

    Update webmap2.js

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,10 +1,10 @@
 let us = L.map('webmap2').setView([38, -100], 4.4);
 	L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(us);
-	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer?request=GetCapabilities&service=WMS', {
+	L.tileLayer.wms('https://mesonet.agron.iastate.edu/wms/radar.php?', {
   		layers: '1',
   		format: 'image/png',
   		transparent: true,
-  		attribution: 'NOAA'
+  		attribution: 'NOAA, Iowa State University'
 	}).addTo(us)
 	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer?request=GetCapabilities&service=WMS', {
  		 layers: '2',
