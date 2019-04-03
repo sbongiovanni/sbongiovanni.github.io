cc52d333da44f6de766b485134e8a4e66e111764
Sarah Bongiovanni <48561808+sbongiovanni@users.noreply.github.com>
2019-04-02 20:43:31 -0500

    Update webmap2.js

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -6,8 +6,8 @@ let us = L.map('webmap2').setView([38, -100], 4.4);
   		transparent: true,
   		attribution: 'NOAA'
 	}).addTo(us)
-	L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/mrms_nn.cgi?', {
- 		 layers: 'mrms_p72h',
+	L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r-t.cgi?', {
+ 		 layers: 'nexrad-n0r-wmst',
  		 format: 'image/png',
   		transparent: true,
   attribution: 'NOAA, Iowa State University'
