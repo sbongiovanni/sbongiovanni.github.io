cc5082b6d194edd209140577dc44f558090eb84e
Sarah Bongiovanni <48561808+sbongiovanni@users.noreply.github.com>
2019-04-02 20:33:47 -0500

    Update webmap2.js

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,15 +1,14 @@
 let us = L.map('webmap2').setView([38, -100], 4.4);
 	L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(us);
-	L.tileLayer.wms('ttps://mesonet.agron.iastate.edu/cgi-bin/wms/goes/west_ir.cgi?', {
-  		layers: 'goes-west-ir-4km-900913',
-  		format: 'image/png',
-  		transparent: true,
-  		attribution: 'NOAA, Iowa State University'
-	}).addTo(us)
 	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_ocean_sfc_sst_time/MapServer/WMSServer', {
   		layers: '1',
   		format: 'image/png',
   		transparent: true,
   		attribution: 'NOAA'
 	}).addTo(us)
+	L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/mrms_nn.cgi?', {
+ 		 layers: 'mrms_p72h',
+ 		 format: 'image/png',
+  		transparent: true,
+  attribution: 'NOAA, Iowa State University'
 }).addTo(us);
