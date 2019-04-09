e1471137c9b513ba056646302fbe0a1239101772
Sarah Bongiovanni <Sarah@Sarahs-MacBook-Pro-2.local>
2019-04-08 21:37:16 -0500

    updated

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -2,12 +2,22 @@ let us = L.map('webmap3').setView([30, -90], 12);
 	L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(us);
 let bikeshareareaURL = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_1.geojson'
 jQuery.getJSON(bikeshareareaURL, function (data) {
-	let areaStyle = { color: 'green' }
-	let areaGeojsonOptions = { style: areaStyle }
-	L.geoJSON(data, areaGeojsonOptions).addTo(us)
+  L.geoJSON(data).addTo(us)
 })
 
 let bikesharestationsURL = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_0.geojson'
 jQuery.getJSON(bikesharestationsURL, function (data) {
-  L.geoJSON(data,).addTo(us)
-})
+    let onEachFeature = function (feature, layer) {
+      let stationnumber = feature.properties.Station_Name;
+      let rackspaces = feature.properties.Number_of_Rack_Spaces;
+      let sign = feature.properties.Sign_Type;
+       layer.bindPopup( stationnumber + ' ' + ': ' + sign + 'Number of Rack Spaces: ' + rackspaces + ' ' )
+  };
+  let parksGeojsonOptions = {
+    style: stateStyle,
+    onEachFeature: onEachFeature
+  };
+  L.geoJSON(data, parksGeojsonOptions).addTo(us);
+	})
+
+bikeshareareaURL.bindPopup('Bike Share Service Area')
