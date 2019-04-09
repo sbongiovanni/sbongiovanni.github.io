ac8c3de8b4b3aff2eeae45a637897b0220b40783
Sarah Bongiovanni <48561808+sbongiovanni@users.noreply.github.com>
2019-04-08 21:06:29 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,11 +1,11 @@
 let us = L.map('webmap3').setView([30, -90], 12);
 	L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(us);
-let stateDemographicsUrl = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_1.geojson'
-let stateStyle = { color: 'green' }
-let stateGeojsonOptions = { style: stateStyle }
-L.geoJSON(data, stateGeojsonOptions).addTo(us)
+let bikeshareareaURL = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_1.geojson'
+let areaStyle = { color: 'green' }
+let areaGeojsonOptions = { style: areaStyle }
+L.geoJSON(data, areaGeojsonOptions).addTo(us)
 
-let stateDemographicsUrl = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_0.geojson'
-jQuery.getJSON(stateDemographicsUrl, function (data) {
+let bikesharestationsURL = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_0.geojson'
+jQuery.getJSON(bikesharestationsURL, function (data) {
   L.geoJSON(data).addTo(us)
 })
