5158960f9bd09ba319781c19c7e6ff678333e80b
Sarah Bongiovanni <Sarah@Sarahs-MacBook-Pro-2.local>
2019-04-08 21:22:37 -0500

    updated

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,11 +1,13 @@
 let us = L.map('webmap3').setView([30, -90], 12);
 	L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(us);
 let bikeshareareaURL = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_1.geojson'
-let areaStyle = { color: 'green' }
-let areaGeojsonOptions = { style: areaStyle }
-L.geoJSON(data, areaGeojsonOptions).addTo(us)
+jQuery.getJSON(bikeshareareaURL, function (data) {
+	let areaStyle = { color: 'green' }
+	let areaGeojsonOptions = { style: areaStyle }
+	L.geoJSON(data, areaGeojsonOptions).addTo(us)
+})
 
 let bikesharestationsURL = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_0.geojson'
 jQuery.getJSON(bikesharestationsURL, function (data) {
-  L.geoJSON(data).addTo(us)
+  L.geoJSON(data,).addTo(us)
 })
