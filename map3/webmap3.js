7aa71d80c9d2fca092e1a29444f4b721029dcd97
Sarah Bongiovanni <48561808+sbongiovanni@users.noreply.github.com>
2019-04-08 20:57:59 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,4 +1,4 @@
-let us = L.map('webmap3').setView([30, -90], 10);
+let us = L.map('webmap3').setView([30, -90], 12);
 	L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(us);
 let stateDemographicsUrl = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_1.geojson'
 jQuery.getJSON(stateDemographicsUrl, function (data) {
