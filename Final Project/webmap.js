e6624d30bf060f444fd3cca61dd574b5884cd3ac
Sarah Bongiovanni <Sarah@Sarahs-MacBook-Pro-2.local>
2019-05-03 19:46:15 -0500

    update

--- Final Project/webmap.js
+++ Final Project/webmap.js
@@ -37,6 +37,7 @@ jQuery.getJSON(ebrDemographicsUrl, function (data) {
   }
   L.geoJSON(data, wardGeojsonOptions).addTo(nola)
 })
+
 let firstaidURL = 'https://opendata.arcgis.com/datasets/7c64080061a5480288fe64afd1bcea1d_0.geojson'
 jQuery.getJSON(routesURL, function (data) {
   L.geoJSON(data).addTo(nola)
